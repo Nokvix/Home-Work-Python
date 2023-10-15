c45167c2c47236d6a087e82be77c7ef514bc93d1
Nokvix <igor.gass2003@mail.ru>
2023-10-15 14:11:35 +0500

    Поправить решение задачи 5 модуль 8

--- module8/5. Список списков — 2.py
+++ module8/5. Список списков — 2.py
@@ -10,4 +10,4 @@ def expand_lists(processing_list):
 
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]], [[11, 12, 13], [14, 15], [16, 17, 18]]]
 expanded_list = expand_lists(nice_list)
-print(expanded_list)
+print("Ответ:", expanded_list)
