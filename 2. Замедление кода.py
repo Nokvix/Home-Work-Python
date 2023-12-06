cb789b4be780cf5850fecff83ab076d5aaf5f80b
Nokvix <igor.gass2003@mail.ru>
2023-12-06 20:30:41 +0500

    Решить задачу 2 модуль 14

---
+++ 2. Замедление кода.py
@@ -0,0 +1,29 @@
+import time
+from typing import Callable, Any
+import functools
+
+
+def sleep_time(func: Callable) -> Any:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        time.sleep(3)
+        return func
+
+    return wrapper()
+
+
+def get_number_1_5() -> None:
+    for i in range(1, 6):
+        print(i)
+
+
+get_number_1_5()
+
+
+@sleep_time
+def get_number_6_10() -> None:
+    for i in range(6, 11):
+        print(i)
+
+
+get_number_6_10()
