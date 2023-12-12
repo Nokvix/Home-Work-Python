3073d15dc5a3059e0d53750ec37ac51fae9f345d
Nokvix <igor.gass2003@mail.ru>
2023-12-12 13:53:35 +0500

    Решить задачу 2 модуль 17

---
+++ module17/2. И снова zip.py
@@ -0,0 +1,8 @@
+from typing import List, Tuple
+
+letters: List[str] = ['a', 'b', 'c', 'd', 'e']
+numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+result: List[Tuple[str, int]] = list(map(lambda string, number: (string, number), letters, numbers))
+
+print(result)
